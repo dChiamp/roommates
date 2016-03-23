73b8842c69d00c83b66524e14c9b485623780be0
chencaleb <clb_chen@berkeley.edu>
2016-03-22 17:51:08 -0700

    mongoose connection

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-var usersController = require("../controllers/usersController")
+var usersController = require("../controllers/usersController");
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
