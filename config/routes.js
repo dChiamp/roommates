377dcf99b935a1e708ebe6e52198900cd9d6ae03
dchiamp <derek.chiampas@gmail.com>
2016-03-22 21:54:01 -0700

    adds home routes

--- config/routes.js
+++ config/routes.js
@@ -3,20 +3,38 @@ var router = express.Router();
 
 var usersController = require("../controllers/usersController");
 
+// LANDING PG
 router.route('/')
   .get(usersController.index);
+
+// USERS
 
 router.route('/api/users')
   .get(usersController.index)
   .post(usersController.createUser);
 
-router.route('/api/users/:id/edit')
-  .get(usersController.edit);
 
 router.route('/api/users/:id')
   .get(usersController.showUser)
-	.put(usersController.updateUser)
+  .put(usersController.updateUser)
   .delete(usersController.deleteUser);
 
+router.route('/api/users/:id/edit')
+  // should be put not get?
+  .get(usersController.edit);
+
+// HOME
+
+router.route('/api/homes')
+  // index for testing
+  .get(homesController.indexHomes)
+
+route.route('api/home/:id')
+  .get(homesController.showHome)
+  .put(homesController.updateHome)
+  .delete(homesController.deleteHome);
+
+route.route('api/home/:id/edit')
+  .get(homesController.edit)
 
 module.exports = router;
